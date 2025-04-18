b1df399d7b517ba281fe5c75cdddc5ba2c1a1226
RoryTeet <rteeter@ucsc.edu>
2025-04-18 12:49:22 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 - Pseudocoding
+// Author: Rory Teeter (rteeter@ucsc.edu)
+// Date: 04/18/2025
+
+//How to play Rock-Paper-Scissors:
+//You and your partner start by placing your fist into your palm.
+  //Then count to three and pound your fist into your palm on every count.
+  //After three say, shoot aloud, and on shoot, pick either a scissors hand position, a fist for a rock, or a resting hand for paper. 
+//Each partner will either have the same or a different position: scissors get broken by rock, paper covers rock, and scissors cut paper. 
+//Play three rounds, and the best out of those rounds wins the game! 
+
+
 
 // Constants
 
