1cbaaaa284c092f7eb5a92db8858181257584cf1
RoryTeet <rteeter@ucsc.edu>
2025-04-24 14:21:16 -0700

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,9 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Use JS to create variables
+// Author: Rory Teeter
+// Date: 04-24-25
 
-// Constants
+//Define Variables:
+make = "Metro Bus";
+model = "city bus";
+color = "blue, white";
+year = 2014;
 
+//Calculate
+age = 2025 - year
+
+//output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "years<br>");
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
