3d8ed6f6806d1eed9fa229e2170cb0d651bfcaa9
RoryTeet <rteeter@ucsc.edu>
2025-05-11 20:22:52 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,10 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 9 JQuery and Libraries
+// Author: Rory Teeter
+// Date: May 11th, 2025
 
 // Constants
 
 // Functions
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+
+  // now add (or subtract) the "special" class to the section
+  $("#challenge").toggleClass("special");
+});
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
