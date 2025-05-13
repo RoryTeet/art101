f4137f8cdfd5cd7d198050fb857d95f92f06e6d8
RoryTeet <rteeter@ucsc.edu>
2025-05-13 12:02:35 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -11,10 +11,12 @@ $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
 // add a click listener to the challenge button
 $("#button-challenge").click(function(){
-
+  console.log("clicked");
+  
   // now add (or subtract) the "special" class to the section
   $("#challenge").toggleClass("special");
 });
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
