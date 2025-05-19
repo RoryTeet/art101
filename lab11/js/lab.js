1a7ee392c6e81e767ce138edca653dba2720832b
RoryTeet <rteeter@ucsc.edu>
2025-05-18 21:23:09 -0700

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,16 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 11 we are making an events and fors
+// Author: Rory Teeter
+// Date: May 18th, 2025
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
+
+// click listener for button
+$("#submit").click(function() {
+  // get value of input field
+  const userName = $("#user-name").val();
+
+  // now let's sort it
+  const userNameSorted = sortString(userName);
+
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
+
 
 function main() {
   console.log("Main function started.");
