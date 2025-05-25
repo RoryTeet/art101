6e5d3c36daf8f16ae9ba50934b837a3258a5a3c6
RoryTeet <rteeter@ucsc.edu>
2025-05-25 15:16:02 -0700

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,21 +1,42 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 13 make a loop
+// Author: Rory Teeter
+// Date: May 26th 2025
 
 // Constants
 
 // Functions
+// Use a for loop to print all the numbers from 1 to 100. Test and debug.
+function whatHappensOnClick() {
+    console.log("click");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = ""; 
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+    for (let i = 1; i <= 200; i++) {
+      let message = "";
+
+      if (i % 7 === 0 && i % 2 === 0) {
+        message = "Boom!";
+      } else if (i % 5 === 0) {
+        message = "Buzz!";
+      } else if (i % 3 === 0) {
+        message = "Fizz!";
+      } else {
+        message = i;
+      }
+
+      const p = document.createElement("p");
+      p.textContent = message;
+      outputDiv.appendChild(p);
+    }
+  }
+
+  // Attach the click event using jQuery
+  $(document).ready(function () {
+    $("#button").click(whatHappensOnClick);
+  });
+
+
 
 // let's get this party started
 main();
